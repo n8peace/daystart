8423c723b4850aac02f8191c9ba20e7278753e06
n8peace <n8peace@gmail.com>
2025-10-20 22:05:44 -0700

    Update shared daystart edge function and changelog

--- supabase/functions/get_shared_daystart/index.ts
+++ supabase/functions/get_shared_daystart/index.ts
@@ -5,7 +5,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 const corsHeaders = {
   'Access-Control-Allow-Origin': 'https://daystartai.app',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type'
+  'Access-Control-Allow-Headers': 'Content-Type, authorization'
 }
 
 serve(async (req) => {
